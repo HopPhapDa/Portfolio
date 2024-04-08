4b214a563cd2759f09f2433de2b6ad2864d44750
HopPhapDa <165137016+HopPhapDa@users.noreply.github.com>
2024-04-08 22:49:13 +0700

    Add files via upload

---
+++ js/custom-carousel.js
@@ -0,0 +1,20 @@
+// JavaScript Document
+$('#banner-me').owlCarousel({
+   	 loop:true,	// cho lap lại
+	items:1,	// xuất hiện 1 ảnh thôi
+	smartSpeed:1000,   	// tốc độ thay đổi ảnh
+	autoplay:true,		// cho phép tự động chạy
+    	autoplayTimeout:5000,  // thời gian chờ khi chuyển ảnh khi chạy tự động
+nav:true,	// cho xuất hiện bộ nút tới lui 
+	  })
+$('#tes').owlCarousel({ loop:true, margin:80, responsive:{ 0:{ items:1, 		 }, 600:{ items:2, 			dots:false, 			nav:true, }, 1000:{ items:3 } } })
+ 
+$('#banner-danhgia').owlCarousel({
+   	 loop:true,	// cho lap lại
+	items:1,	// xuất hiện 1 ảnh thôi
+	smartSpeed:1000,   	// tốc độ thay đổi ảnh
+	autoplay:true,		// cho phép tự động chạy
+    	autoplayTimeout:5000,  // thời gian chờ khi chuyển ảnh khi chạy tự động
+nav:true,	// cho xuất hiện bộ nút tới lui 
+	  })
+$('#tes').owlCarousel({ loop:true, margin:80, responsive:{ 0:{ items:1, 		 }, 600:{ items:2, 			dots:false, // JavaScript Document
---
+++ js/custom.js
@@ -0,0 +1,36 @@
+// JavaScript Document
+$(document).ready(function(){
+	$("#bat-tat-menu").click(function(){
+		$(".menu").slideToggle()
+	})
+})
+new WOW().init();
+
+var typed = new Typed('.element', {
+               	strings: ["Web Designer.", "Code Developer.","Apps Designer."],
+       	 	typeSpeed:50,
+        		backSpeed:50,
+        		loop:true,
+        		});
+
+
+function openCity(evt, cityName) {
+  var i, tabcontent, tablinks;
+  tabcontent = document.getElementsByClassName("tabcontent");
+  for (i = 0; i < tabcontent.length; i++) {
+    tabcontent[i].style.display = "none";
+  }
+  tablinks = document.getElementsByClassName("tablinks");
+  for (i = 0; i < tablinks.length; i++) {
+    tablinks[i].className = tablinks[i].className.replace(" active", "");
+  }
+  document.getElementById(cityName).style.display = "block";
+  evt.currentTarget.className += " active";
+}
+
+// Get the element with id="defaultOpen" and click on it
+document.getElementById("defaultOpen").click();
+
+
+
+// JavaScript Document
